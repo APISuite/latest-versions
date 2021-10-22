4b477d33dd9b0c13c6a51b33a23935d1b5c0f71c
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-10-22 14:47:42 +0100

    latest versions cli tool setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## APISuite Latest Versions
+
+Simple tool to print the recent version of all APISuite's relevante packages.
+
+Requires a GitHub Personal Access Token with full repository access.
+
+Usage: `PAT=[token] npm start`
---
+++ index.js
@@ -0,0 +1,46 @@
+const { Octokit } = require('@octokit/core')
+let octokit
+
+async function getLatestRelease(repo) {
+    const release = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+        owner: 'APISuite',
+        repo,
+    })
+    return {
+        repo,
+        tag: release.data.tag_name,
+    }
+}
+
+async function printReleases() {
+    const releases = await Promise.all([
+        getLatestRelease('apisuite-be'),
+        getLatestRelease('apisuite-fe'),
+        getLatestRelease('apisuite-billing-extension'),
+        getLatestRelease('apisuite-billing-extension-ui'),
+        getLatestRelease('apisuite-marketplace-extension'),
+        getLatestRelease('apisuite-marketplace-extension-ui'),
+        getLatestRelease('apisuite-cloud-extension-ui'),
+    ])
+
+    for (const r of releases) {
+        console.log(r.repo, r.tag)
+    }
+}
+
+async function run() {
+    try {
+        if (!process.env.PAT) {
+            console.log('Missing personal access token in env var PAT')
+            process.exit(1)
+        }
+
+        octokit = new Octokit({ auth: process.env.PAT });
+
+        await printReleases()
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+run()
